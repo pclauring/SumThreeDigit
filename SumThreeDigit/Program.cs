2ce6dcfccf72e55e02e2a85a1ffd897496c62f30
Pierce Lauring <pclauring@gmail.com>
2017-10-12 15:49:31 -0400

    added a while loop to verify the selection integers of the same length

--- SumThreeDigit/Program.cs
+++ SumThreeDigit/Program.cs
@@ -7,48 +7,52 @@ namespace SumThreeDigit
         static void Main(string[] args)
         {
             int valueForCompare1, valueForCompare2;
-
-            Console.WriteLine("Please enter your first positive integer for comparison");
-
-            //while loop with TryParse to find a value that can be converted into an int
-            valueForCompare1 = 0;
-            while (!int.TryParse(Console.ReadLine(), out valueForCompare1))
+            bool digitsTheSame = true;
+            while (digitsTheSame)
             {
-                Console.WriteLine("The value could not be converted to an int!");
-                Console.WriteLine("Please enter a valid integer");
+                digitsTheSame = true;
+                Console.WriteLine("Please enter your first positive integer for comparison");
+
+                //while loop with TryParse to find a value that can be converted into an int
+                valueForCompare1 = 0;
+                while (!int.TryParse(Console.ReadLine(), out valueForCompare1))
+                {
+                    Console.WriteLine("The value could not be converted to an int!");
+                    Console.WriteLine("Please enter a valid integer");
+                }
+
+                // Convert integer to a string
+                string valueString1 = Convert.ToString(valueForCompare1);
+                Console.WriteLine("Integer Length : {0}", valueString1.Length);
+
+                Console.WriteLine("Value 1 = {0}", valueForCompare1);
+
+                Console.WriteLine("Please enter your second positive integer for comparison");
+
+                //while loop with TryParse to find a value that can be converted into an int
+                valueForCompare2 = 0;
+                while (!int.TryParse(Console.ReadLine(), out valueForCompare2))
+                {
+                    Console.WriteLine("The value could not be converted to an int!");
+                    Console.WriteLine("Please enter a valid integer");
+                }
+
+                // Convert integer to a string
+                string valueString2 = Convert.ToString(valueForCompare2);
+                Console.WriteLine("Integer Length : {0}", valueString1.Length);
+
+                Console.WriteLine("Value 2 = {0}", valueForCompare2);
+
+                if (valueString1.Length == valueString2.Length)
+                {
+                    digitsTheSame = false;
+                    Console.WriteLine(SumComparison(valueString1, valueString2));
+                }
+                else
+                {
+                    Console.WriteLine("Integers are not the same length!");
+                }
             }
-
-            // Convert integer to a string
-            string valueString1 = Convert.ToString(valueForCompare1);
-            Console.WriteLine("Integer Length : {0}", valueString1.Length);
-
-            Console.WriteLine("Value 1 = {0}", valueForCompare1);
-
-            Console.WriteLine("Please enter your second positive integer for comparison");
-
-            //while loop with TryParse to find a value that can be converted into an int
-            valueForCompare2 = 0;
-            while (!int.TryParse(Console.ReadLine(), out valueForCompare2))
-            {
-                Console.WriteLine("The value could not be converted to an int!");
-                Console.WriteLine("Please enter a valid integer");
-            }
-
-            // Convert integer to a string
-            string valueString2 = Convert.ToString(valueForCompare2);
-            Console.WriteLine("Integer Length : {0}", valueString1.Length);
-
-            Console.WriteLine("Value 2 = {0}", valueForCompare2);
-
-            if (valueString1.Length == valueString2.Length)
-            {
-                Console.WriteLine(SumComparison(valueString1,valueString2));
-            }
-            else
-            {
-                Console.WriteLine("Strings are not the same length");
-            }
-
         }
         //checks the sums equality and prints the answer
         public static bool SumComparison(string x, string y)
@@ -62,7 +66,7 @@ namespace SumThreeDigit
                     return false;
                 }
             }
-                   return true;
-            }
+            return true;
         }
+    }
 }
